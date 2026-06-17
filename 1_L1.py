6cb93b4fc5c4c70efeceffd3c6ca77ec88305528
Akshat Bhushan <ak2111bh@gmail.com>
2026-06-17 20:01:22 +0530

    first push

---
+++ 1_L1.py
@@ -0,0 +1,25 @@
+# a=1
+# b=2
+# print (a+b)
+# c = 22/2
+# print(c)
+
+# """jjk"""
+
+# panda = "cute"
+# print (panda[2],panda[0])
+
+# print (type(a))
+# print (type(c))
+# print (type(panda))
+
+# print ("hero"[2],"hero"[1])
+
+a = "hello I am Data Scientist"
+#Extract Hello 
+#Extract Data 
+#extract Scientist
+
+print (a[:5])
+print (a[11:15])
+print (a[16:26])
