40c7afa91d7ce3578b753afe13545de5198bd551
Akshat Bhushan <ak2111bh@gmail.com>
2026-07-12 20:20:30 +0530

    neg

--- 1_lec2.py
+++ 1_lec2.py
@@ -1,2 +1,65 @@
-# string indexing 
+# # string indexing 
 
+# a = "Benjamin Franklin"
+
+# # print (a[2,4]) illey ❌
+
+
+# # print (a[2], a[4]) 
+
+# # 2.slicing
+
+# # print (a[:10:]) 
+
+# print (a[:10])  # Benjamin F
+# print (a[4::])  # amin Franklin 
+# print (a[4:10:]) # amin F
+# print (a[4:10:]) 
+
+# a = "hello I am Data Scientist"
+# # Hello D S
+# print (a[0:5])
+# print (a[11:15])
+# print (a[16:])
+
+# print("GOne" [1,2])
+
+# Esc seq.
+
+# print ("Hello\r.")
+# print ("HeiyaHero\rHiko")
+# print ("Hello\nHi")
+# print (r'this is hero "time" kachow lets\'s go ')
+
+# print (f'Rev :- {a[::-1]}')
+
+# name = input("Your Name?")
+# age = int(input("Your Birth year?"))
+# month = int(input("Enter your Birth month in number"))
+
+# print (f'Herllo {name}, you are currently {2026-age} years old')
+
+# 🔍 Search.
+
+# j = "coding".find("d")
+# print (j)
+
+# b= "1893"
+# print ("18" in b)
+
+
+# Asg
+
+# arr = [0] * 5
+
+# for i in range(5):
+#     print(str(arr[i]) + " ")
+
+# str = "Ben10 " * 4
+# print (str)
+str = "Ben10"
+print (str[0:1:1])
+print (str[-2:-1:1])
+
+jk = "123".isdigit()
+print (jk)
