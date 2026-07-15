bf4d3239abf9d0b329f47b6ec5d211e4a1cf61bb
Akshat Bhushan <ak2111bh@gmail.com>
2026-07-15 21:46:26 +0530

    Operators

--- Operators/02_Lec03.py
+++ Operators/02_Lec03.py
@@ -1,2 +1,29 @@
 # Arithmetic operators 
 
+# print(12+15-5)
+# print(12-15+5)
+# print(12-15/5)
+# print((12-15)/3)
+# print((20+15)//5)
+# print((20+15)%5)
+
+# a = "Sup"
+# b ="Bro"
+
+# print (a + b , sep="....")  # not with one argument
+# print (a , b , end="...")  
+# print (a , b , end="\n")  
+# print (a , b , sep="....")  #2 arguments here
+# print ("Yo" , "brp" , sep = "...")
+# print ("10 " *10 , sep = "...")
+
+
+
+# a = True
+# print (20<50)
+# print (20<50>=50)
+# print (20<50<30)
+# print ((20<50)==a)
+# print ((37<78)==False)
+# print ((20>50)==False)
+
---
+++ Operators/02_Lec04.py
@@ -0,0 +1,32 @@
+# Logical Operatos
+
+# print ("Hero" or 0)
+
+# Comparison
+
+# print (10 <= 100)
+
+
+# Iden - EXTRAS
+
+# a = 1
+# b = 1
+# c = a
+
+# print (a is b)
+# print (a is c)
+# print (a is not b)
+
+# a = [1, 2]
+# b = a
+# c = [1, 2]
+
+# print(a is b)     
+# print(a is c)      
+# print(a == c)      
+# print(a is not c)      
+# print ("p" in "python")
+
+
+
+ 
