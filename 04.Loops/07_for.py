d9ee54b5f34260de92359ccf1f7521a4acfe4324
Akshat Bhushan <ak2111bh@gmail.com>
2026-07-19 03:47:33 +0530

    For

---
+++ 04.Loops/07_for.py
@@ -0,0 +1,31 @@
+# for i in range(11):
+#     print(i)
+
+# 10-40
+
+# for i in range(10,41):
+#     print(i)
+
+# for i in range(-10,21):
+#     print(i)
+
+# for i in range (34,4,-1):
+#     print(i)
+
+# 5 table
+
+# for x in range (5,51,5):
+#     print(x)
+
+# str = "Jujutsu"
+
+# # for i in range(len(str)):
+# #     print(str[i])
+
+# for i in str:
+#     print(i)
+
+z = int(input("Enter a no. to create a table:- "))
+
+for i in range(z,(z*10)+1,z):
+    print(i)
---
+++ 08_for_practice.py
@@ -0,0 +1,117 @@
+# 👉🏻 1. Hello World inp time
+# '''
+# str = int(input("Enter a no. :-"))
+
+# # print ("Hello World\n"*str)
+
+# for i in range(str):
+#     print(f"Hello World! X{i+1}")
+# '''
+
+# 👉🏻 2. print till n 
+
+# n = int(input("Enter a no. :-"))
+# # for i in range(n):
+# #     print(i+1)  
+
+# or 
+
+# for i in range(1,n+1):
+#     print(i)
+
+# 👉🏻 3. inp to 1
+
+# n = int(input("Enter a no. :-"))
+
+# for i in range (n,0,-1):
+#     print(i)
+
+
+# 👉🏻 4. Sum 1 to inp
+
+
+# sum=0
+# n = int(input("Enter a no. :-"))
+
+# for i in range (1,n+1):
+#     sum+=i
+# print(sum)
+
+# 👉🏻 5. factorial of inp
+
+# f=1
+# n = int(input("Enter a no. :-"))
+
+# for i in range(n,1,-1):
+#     f*=i                             #    print(f"Factorial of {n} is {i*(i-1)}")
+    
+# print (f)
+
+# 👉🏻 6. Even odd sum till inp
+
+# odd= 0
+# even = 0
+# a =int(input("Enter a No. Range :-"))
+
+# for i in range (1,a+1):
+#     if i%2==0:
+#         even+=i    # not a cause it will add lets say start num 5 each time
+#     else:
+#         odd+=i
+
+# print(f"Sum of Even no. = {even}")
+# print(f"Sum of Odd no. = {odd}")
+
+# 👉🏻 7. factors
+
+# factor = "" ------ nope
+# num =int(input("Enter the no. to find it's factors :-"))
+# fact= 0
+# for i in range(1,num+1):  # without 1 start it will give error , 0 div error
+#     if num%i==0:
+#         print(i)
+#         fact+=1
+# print(f"Total factors = {fact} ")
+
+
+# 👉🏻 . 8 Factors sum
+
+# fact = 0
+# num =int(input("Enter the no. to find it's factors' sum :-"))
+
+# for i in range (1,num+1):
+#     if num%i==0:
+#         fact+=i
+        
+# print(f"Sum of factors of {num} = {fact} ")
+
+# 👉🏻 9. Power without **
+
+# a = int(input("Enter base:- "))
+# b = int(input("Enter power:- "))
+# p=1
+
+# for i in range (b):
+#     p*=a
+# print(f"{a}^{b} = {p}")
+
+
+
+
+# 👉🏻 10. Prime no.
+
+a = int(input("Enter a no. :- "))
+f= 0
+
+for i in range(1,a+1):
+    if a%i==0:
+        f+=1
+
+if f==1:
+    print("It's a unity no.")    
+elif a==0:
+    print("It's 0, neither prime nor composite")
+elif f==2:
+    print("It's a prime no.")
+else:
+    print("It's a composite no.")
