734361c858151e7aa2f2cbac13a34f10fc476e16
Akshat Bhushan <ak2111bh@gmail.com>
2026-07-22 04:01:45 +0530

    while loops

---
+++ 04.Loops/09_while.py
@@ -0,0 +1,27 @@
+# i = 1
+
+# while i<=40:
+#     print(i)    # Infinite loop without i reassignment
+#     i+=1
+
+# a = 10
+
+# while a>0:
+#     print(a)
+#     a-=1
+
+
+# j = 1
+
+# while j<16:
+#     print (j , "top")
+#     j+=1
+#     if j == 10:
+          # continue
+#         # print (j)
+
+#         break
+#     # print (j , "Bottom")
+
+
+
